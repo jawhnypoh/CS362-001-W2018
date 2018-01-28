87847b7f82aab47de629a86a77e0b8565d1ecaba
jawhnypoh <po.johnny@gmail.com>
2018-01-27 16:58:18 -0800

    Working on TimeTableTest

--- projects/poj/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
+++ projects/poj/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 
 import org.junit.Test;
+import sun.util.calendar.Gregorian;
 
 import static org.junit.Assert.*;
 
@@ -17,8 +18,36 @@ public class TimeTableTest {
 
 	 @Test
 	  public void test01()  throws Throwable  {
-		 
+
+		 TimeTable timeTable = new TimeTable();
+
+		 LinkedList<Appt> appts = new LinkedList<Appt>();
+
+		 GregorianCalendar firstDay = new GregorianCalendar(2018, 01, 10);
+		 GregorianCalendar lastDay = new GregorianCalendar(2018, 01, 20);
+
+		 int startHour = 10;
+		 int startMinute = 10;
+		 int startDay = 10;
+		 int startMonth = 10;
+		 int startYear = 10;
+		 String title = "Title";
+		 String description = "Description";
+
+		 //Construct a new Appointment object with the initial data
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+
+		 // Assertions
+		 assertNotNull(timeTable.getApptRange(appts, firstDay, lastDay));
+		 assertNotNull(timeTable.deleteAppt(appts, appt));
 	 }
+
 	 @Test
 	  public void test02()  throws Throwable  {
 		 
