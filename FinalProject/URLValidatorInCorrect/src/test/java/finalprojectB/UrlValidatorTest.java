7480350f8ada254b029bd25caebde1d6c1a5ea3e
jawhnypoh <po.johnny@gmail.com>
2018-03-18 01:53:40 -0700

    Adding initial code for UrlValidatorTest

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -22,19 +22,50 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+      //You can use this function to implement your manual testing
+      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+      // Manual testing
+      assertTrue(urlVal.isValid("http://www.google.com"));
+      assertTrue(urlVal.isValid("http://www.maps.google.com/"));
+      assertTrue(urlVal.isValid("ftp://go.com:0/test1/file?action=view"));
+      assertTrue(urlVal.isValid("ftp://go.com:0/test1/file?action=view"));
+      assertTrue(urlVal.isValid("http://255.com/test1/file"));
+      assertTrue(urlVal.isValid("http://225.com"));
 	   
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your First Partition testing
+      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+      // Missing part of URL
+      assertFalse(urlVal.isValid("http//:8080"));
+      assertFalse(urlVal.isValid("http:google/aaa"));
+      assertFalse(urlVal.isValid("ftp:80/test#/read"));
+      assertTrue(urlVal.isValid("http://www.google.com/test"));
+
+      // Bad part of URL
+      assertFalse(urlVal.isValid("htp:google.com"));
+      assertFalse(urlVal.isValid("http://aaa:-10/file"));
+      assertFalse(urlVal.isValid("http:.com"));
 
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+      //You can use this function to implement your Second Partition testing
+      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+      // Good URL partitions
+      assertTrue(urlVal.isValid("http://www.google.com/$23?action=edit&mode=up"));
+      assertTrue(urlVal.isValid("h3t://255.com/t123?action=edit&mode=up"));
+
+      // Bad URL partitions
+      assertFalse(urlVal.isValid("go://256.256.256.256:65c/../"));
+      assertFalse(urlVal.isValid("://abc.:-50/..//file/../"));
+      assertFalse(urlVal.isValid("h3t://256:-50/..//c"));
 
    }
    //You need to create more test cases for your Partitions if you need to 
@@ -44,7 +75,97 @@ public class UrlValidatorTest extends TestCase {
 	   //You can use this function for programming based testing
 
    }
-   
+
+//-------------------- Test data for creating a composite URL
+   /**
+    * The data given below approximates the 4 parts of a URL
+    * <scheme>://<authority><path>?<query> except that the port number
+    * is broken out of authority to increase the number of permutations.
+    * A complete URL is composed of a scheme+authority+port+path+query,
+    * all of which must be individually valid for the entire URL to be considered
+    * valid.
+    */
+   ResultPair[] testUrlScheme = {new ResultPair("http://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("h3t://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)};
+
+   ResultPair[] testUrlAuthority = {new ResultPair("www.google.com", true),
+           new ResultPair("go.com", true),
+           new ResultPair("go.au", true),
+           new ResultPair("0.0.0.0", true),
+           new ResultPair("255.255.255.255", true),
+           new ResultPair("256.256.256.256", false),
+           new ResultPair("255.com", true),
+           new ResultPair("1.2.3.4.5", false),
+           new ResultPair("1.2.3.4.", false),
+           new ResultPair("1.2.3", false),
+           new ResultPair(".1.2.3.4", false),
+           new ResultPair("go.a", false),
+           new ResultPair("go.a1a", false),
+           new ResultPair("go.1aa", false),
+           new ResultPair("aaa.", false),
+           new ResultPair(".aaa", false),
+           new ResultPair("aaa", false),
+           new ResultPair("", false)
+   };
+   ResultPair[] testUrlPort = {new ResultPair(":80", true),
+           new ResultPair(":65535", true),
+           new ResultPair(":0", true),
+           new ResultPair("", true),
+           new ResultPair(":-1", false),
+           new ResultPair(":65636",false),
+           new ResultPair(":65a", false)
+   };
+   ResultPair[] testPath = {new ResultPair("/test1", true),
+           new ResultPair("/t123", true),
+           new ResultPair("/$23", true),
+           new ResultPair("/..", false),
+           new ResultPair("/../", false),
+           new ResultPair("/test1/", true),
+           new ResultPair("", true),
+           new ResultPair("/test1/file", true),
+           new ResultPair("/..//file", false),
+           new ResultPair("/test1//file", false)
+   };
+   //Test allow2slash, noFragment
+   ResultPair[] testUrlPathOptions = {new ResultPair("/test1", true),
+           new ResultPair("/t123", true),
+           new ResultPair("/$23", true),
+           new ResultPair("/..", false),
+           new ResultPair("/../", false),
+           new ResultPair("/test1/", true),
+           new ResultPair("/#", false),
+           new ResultPair("", true),
+           new ResultPair("/test1/file", true),
+           new ResultPair("/t123/file", true),
+           new ResultPair("/$23/file", true),
+           new ResultPair("/../file", false),
+           new ResultPair("/..//file", false),
+           new ResultPair("/test1//file", true),
+           new ResultPair("/#/file", false)
+   };
+
+   ResultPair[] testUrlQuery = {new ResultPair("?action=view", true),
+           new ResultPair("?action=edit&mode=up", true),
+           new ResultPair("", true)
+   };
+
+   Object[] testUrlParts = {testUrlScheme, testUrlAuthority, testUrlPort, testPath, testUrlQuery};
+   Object[] testUrlPartsOptions = {testUrlScheme, testUrlAuthority, testUrlPort, testUrlPathOptions, testUrlQuery};
+   int[] testPartsIndex = {0, 0, 0, 0, 0};
+
+   //---------------- Test data for individual url parts ----------------
+   ResultPair[] testScheme = {new ResultPair("http", true),
+           new ResultPair("ftp", false),
+           new ResultPair("httpd", false),
+           new ResultPair("telnet", false)};
 
 
 }
+
